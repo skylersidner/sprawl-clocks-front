b2ee7ee44b9c35ddca4ab2c6c183dfaee0b879de
Skyler Sidner <skyler.sidner@atssecured.com>
2017-07-08 11:39:17 -0500

    first commit after yeoman generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sprawlClocksFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sprawlClocksFrontApp
+ */
+angular.module('sprawlClocksFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
